53452ec7ffe852ee37074382dc3edf33ec934cd6
Abhay Kulshrestha <abhaykul2001@gmail.com>
2020-10-29 23:25:03 +0530

    Overloading

---
+++ OOP_7.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Human{
+    private:
+        int age;
+        string name;
+    public:
+    Human(){
+        cout<<"default constructor"<<endl;
+        age = 0;
+        name = "noname";
+    }
+    Human(string iname){
+        cout<<"constructor with name as the parameter"<<endl;
+        age = 0;
+        name = iname;
+    }
+    Human(int iage){
+        cout<<"constructor with age as the parameter"<<endl;
+        age = iage;;
+        name = "noname";
+    }
+    Human(string iname,int iage){
+        cout<<"constructor with name and age as the parameter"<<endl;
+        age = iage;
+        name = iname;
+    }
+    void display(){
+        cout<<name<<endl<<age<<endl;
+    }
+};
+
+int main(){
+    Human anil;
+    anil.display();
+
+    Human andy("andy");
+    anil.display();
+
+    Human abhay(29);
+    anil.display();
+
+    Human shyam("shyam",25);
+    shyam.display();
+
+    return 0;
+}
