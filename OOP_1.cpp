5b0e1aa52ce200a80318eb6fbf24a96df457a000
Abhay Kulshrestha <abhaykul2001@gmail.com>
2020-10-29 22:09:51 +0530

    OOP simple program

---
+++ OOP_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class HumanBeing{
+public : 
+    void display(){
+       cout<<"Hello I am a human being";
+   }
+
+};
+
+
+int main(){
+    
+    HumanBeing anil;
+    anil.display();
+    return 0;
+
+}
